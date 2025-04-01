37cb36b5c22e068478d064ae8b13dbd356053eca
H82M19\Student <Student>
2025-04-01 19:48:48 +0700

    Su dung public api de the hien danh sach nguoi dung

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+/*import PostList from "./components/PostList";
+function App() {0
+return (
+<div>
+<h1>Ứng dụng Fetch API</h1>
+<PostList />
+</div>
+);
+}
+
+export default App;*/
+
+import { UserProvider } from "./context/UserContext";
+import UserList from "./components/UserList";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <UserProvider>
+      <div className="container mx-auto mt-10">
+        <h1 className="text-2xl font-bold text-center mb-4">
+        Danh sách người dùng
+        </h1>
+        <UserList />
+      </div>
+    </UserProvider>
+  );
+}
+export default App;
